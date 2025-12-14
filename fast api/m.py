b743bbde18959bffe6cb3103e708ac084a4559c9
curiositycrafter <inkcascades@gmail.com>
2025-12-14 16:27:55 +0530

    auto save 16:27:54

--- fast api/m.py
+++ fast api/m.py
@@ -4,4 +4,4 @@ app=FastAPI()
 
 @app.get('/') #used to define a path in fast api 
 
-# its an app decorater and it defines 
+# its an app decorater and it defines a path for the HTTP GET method and path is / which is root directory
