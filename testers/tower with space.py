2a2fc78f5b3e46e0e76ce7688c31e7a5f4a7cc09
curiositycrafter <inkcascades@gmail.com>
2025-12-20 14:07:41 +0530

    auto save 14:07:40

--- testers/tower with space.py
+++ testers/tower with space.py
@@ -1,7 +1,7 @@
 n=8
 for row in range(n):
     for col in range(n):
-        if col>=n-row-1 and  (col-row-1)%2==0:
+        if col>=n-row-1 and  (col-row-1)%2==0 and col<:
             print('*',end='')
         else:
             print(' ',end='')
