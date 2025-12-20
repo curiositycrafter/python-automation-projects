064c3053789e1f0d783f434339cfd909492bdd1e
curiositycrafter <inkcascades@gmail.com>
2025-12-20 13:45:07 +0530

    auto save 13:45:07

--- testers/tower with space.py
+++ testers/tower with space.py
@@ -1,7 +1,7 @@
 n=8
 for i in range(n):
     for j in range(n):
-        if j>=n-i-1 and j:
+        if j>=n-i-1 and j-n-i-1:
             print('*',end='')
         else:
             print(' ',end='')
