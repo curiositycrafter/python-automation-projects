1823fae147f7d829b2728870b4e7f600eace351d
curiositycrafter <inkcascades@gmail.com>
2025-12-20 13:46:17 +0530

    auto save 13:46:17

--- testers/tower with space.py
+++ testers/tower with space.py
@@ -1,7 +1,7 @@
 n=8
 for i in range(n):
     for j in range(n):
-        if j>=n-i-1 or:
+        if j>=n-i-1 and  (j-n-i-1)%2==0:
             print('*',end='')
         else:
             print(' ',end='')
